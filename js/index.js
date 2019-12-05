07ff514ff1c3e5b7f56472fed08f3ca91dc2aabf
AvMkhitaryan <av_mkhitaryan@bk.ru>
2019-12-05 17:12:33 +0400

    buttstrap parent id

--- js/index.js
+++ js/index.js
@@ -1,94 +1,28 @@
-function fav() {
-    var icon = document.getElementById("favIcon");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
+// function fav(argumnet) {
+//     var icon = document.getElementById("favIcon"+ (+i + 1));
+//     console.log(icon);
+//     if (argument === i) {
+//         if (icon.classList.contains("fa-minus")) {
+//             icon.classList.remove("fa-minus");
+//             icon.classList.add("fa-plus");
+//         } else {
+//             icon.classList.remove("fa-plus");
+//             icon.classList.add("fa-minus");
+//         }
+//     }
+// }
 
-function fav1() {
-    var icon = document.getElementById("favIcon1");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
-
-function fav2() {
-    var icon = document.getElementById("favIcon2");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
-
-function fav3() {
-    var icon = document.getElementById("favIcon3");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
-
-function fav11() {
-    var icon = document.getElementById("favIcon11");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
-
-function fav22() {
-    var icon = document.getElementById("favIcon22");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
-
-function fav33() {
-    var icon = document.getElementById("favIcon33");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
-
-function fav44() {
-    var icon = document.getElementById("favIcon44");
-    if (icon.classList.contains("fa-minus")) {
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-    } else {
-        icon.classList.remove("fa-plus");
-        icon.classList.add("fa-minus");
-    }
-}
+// function fav(argument) {
+//     if (argument === 1) {
+        // let icon_plus = document.getElementsByTagName('i')[0];
+        // let btn = document.getElementById('favBtn1');
+        // btn.setAttribute('class', 'btn btn-link fa fa-minus');
+    // }
+// }
 
 
-function MyFUN1(arg) {
-    var container = document.getElementsByClassName('col_7_col_5')
+function myFUN1(arg) {
+    var container = document.getElementsByClassName('col_7_col_5');
     if (arg === 0) {
         container[0].innerHTML = `
                             <div class="row text_block1 ">
@@ -316,51 +250,32 @@ function MyFUN1(arg) {
 
         `
     }
-    // var x = document.querySelector(".text_block1");
-    // var z = document.querySelector(".text_none1");
-    // if (x.style.display === "none") {
-    //     x.style.display = "block";
-    //     z.style.display = "none";
-    // } else {
-    //     x.style.display = "none";
-    //     z.style.display = "block";
-    // }
-}
+    if (arg === 5) {
+        container[0].innerHTML = `
+                            <div class="row text_none2">
+                            <h1>Table 5</h1>
+                            <table style="width:100%">
+                                <tr>
+                                    <th>Fir255stname</th>
+                                    <th>Las5gfh2tname</th>
+                                    <th>Ag55e</th>
+                                </tr>
+                                <tr>
+                                    <td>Ji55ll</td>
+                                    <td>Smi55gh2th</td>
+                                    <td>5055</td>
+                                </tr>
+                                <tr>
+                                    <td>E552ve</td>
+                                    <td>Ja55ckson</td>
+                                    <td>9554</td>
+                                </tr>
+                            </table>
+                        </div>
 
-function MyFUN2() {
-    var x = document.querySelector(".text_block1");
-    var z = document.querySelector(".text_none2");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        z.style.display = "none";
-    } else {
-        x.style.display = "none";
-        z.style.display = "block";
+        `
     }
-}
 
-function MyFUN3() {
-    var x = document.querySelector(".text_block1");
-    var z = document.querySelector(".text_none3");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        z.style.display = "none";
-    } else {
-        x.style.display = "none";
-        z.style.display = "block";
-    }
-}
-
-function MyFUN4() {
-    var x = document.querySelector(".text_block1");
-    var z = document.querySelector(".text_none4");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        z.style.display = "none";
-    } else {
-        x.style.display = "none";
-        z.style.display = "block";
-    }
 }
 
 
@@ -368,14 +283,119 @@ var arr = ["Базы данных", "SQL", "Состояние", "Учетные
 var parent = document.getElementById('nav_li');
 
 for (var i in arr) {
+
     var li = document.createElement('li');
-    // li.innerHTML = i;
-    parent.appendChild(li);
     li.setAttribute('class', 'nav-item');
-    let but = document.createElement('button');
+    parent.appendChild(li);
+
+    var but = document.createElement('button');
     but.setAttribute('class', 'btn btn-outline-dark btn_style');
     but.setAttribute('type', 'button');
     but.innerText = arr[i];
     li.appendChild(but);
 
- }
+}
+
+var Dtb = ["database1", "database2", "database3", "database4", "database5"];
+var par = document.getElementById("col_2_div");
+
+for (var i in Dtb) {
+    var div = document.createElement("div");
+    par.appendChild(div);
+
+    var button1 = document.createElement("button");
+    button1.setAttribute('type', 'button');
+    button1.setAttribute('class', 'btn btn-link fa fa-plus icon_cl test');
+    button1.setAttribute('data-toggle', 'collapse');
+    button1.setAttribute('data-target', '#demo' + i);
+    button1.setAttribute('id', 'favBtn' + (+i + 1));
+    div.appendChild(button1);
+
+    var icon = document.createElement("i");
+    icon.setAttribute('id', 'favIcon' + (+i + 1));
+    icon.setAttribute('class', 'test');
+    button1.appendChild(icon);
+
+    var button2 = document.createElement("button");
+    button2.setAttribute('type', 'button');
+    button2.setAttribute('class', 'btn btn-link');
+    button2.setAttribute('onclick', 'myFUN1(' + (+i + 1) + ')');
+    button2.innerHTML = Dtb[i];
+    button1.appendChild(button2);
+
+    var div1 = document.createElement('div');
+    div1.setAttribute('id', `demo${i}`);
+    div1.setAttribute('class', "collapse clap");
+    par.appendChild(div1);
+
+    var div2 = document.createElement("div");
+    div2.setAttribute("class", "accordion");
+    div2.setAttribute("id", "accordion" + i);
+    div1.appendChild(div2);
+
+    var div3 = document.createElement("div");
+    div3.setAttribute("class", "accordion-group");
+    div2.appendChild(div3);
+
+    var div4 = document.createElement("div");
+    div4.setAttribute("class", "accordion-heading");
+    div3.appendChild(div4);
+
+    var button3 = document.createElement("button");
+    button3.setAttribute("class", "accordion-toggle fa fa-plus icon_cl btn btn-link test1 collapsed");
+    button3.setAttribute("data-toggle", "collapse");
+    button3.setAttribute("data-parent", "#accordion" + (+i));
+    button3.setAttribute("href", "#collapseOneA" + (+i + 1));
+    button3.setAttribute("id", 'favIcon' + (+i + 1));
+    button3.setAttribute("aria-expanded", 'false');
+    // button3.setAttribute("onclick", "fav(" + (+i + 1) + ")");
+    button3.innerHTML = "Text" + (+i + 1);
+    div4.appendChild(button3);
+
+    var div5 = document.createElement("div");
+    div5.setAttribute("id", "collapseOneA" + (+i + 1));
+    div5.setAttribute("class", "accordion-body collapse");
+    div3.appendChild(div5);
+
+    var div6 = document.createElement('div');
+    div6.setAttribute('class', 'accordion-inner');
+    div6.innerHTML = ` <p>Anim pariatur cliche... Anim pariatur cliche... Anim pariatur cliche... Anim pariatur cliche... Anim pariatur cliche... </p>`;
+    div5.appendChild(div6);
+  //   par.innerHTML+=`<div class="accordion" id="accordionExample${i}">
+  //       <div class="card">
+  //       <div class="card-header" id="headingOne${i}">
+  //       <h5 class="mb-0">
+  //       <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne${i}" aria-expanded="true" aria-controls="collapseOne${i}">
+  //       Collapsible Group Item #${i}
+  //   </button>
+  //   </h5>
+  //   </div>
+  //
+  //   <div id="collapseOne${i}" class="collapse" aria-labelledby="headingOne${i}" data-parent="#accordionExample${i}">
+  //       <div class="card-body">
+  //               <div class="card">
+  //   <div class="card-header" id="headingTwo${i}">
+  //     <h5 class="mb-0">
+  //       <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo${i}" aria-expanded="false" aria-controls="collapseTwo${i}">
+  //         Collapsible Group Item Tow#${i}
+  //       </button>
+  //     </h5>
+  //   </div>
+  //   <div id="collapseTwo${i}" class="collapse" aria-labelledby="headingTwo${i}" data-parent="#collapseOne${i}">
+  //     <div class="card-body">
+  //       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+  //     </div>
+  //   </div>
+  // </div>
+  //       </div>
+  //   </div>
+  //   </div>`
+
+
+}
+$(document).on('click', '.test', function () {
+    $(this).toggleClass('fa-minus');
+});
+$(document).on('click', '.test1', function () {
+    $(this).toggleClass('fa-minus');
+});
